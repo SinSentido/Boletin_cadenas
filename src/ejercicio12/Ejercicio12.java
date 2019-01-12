19f9d8a5f91ff464c2f38ab0efe8948180de3c89
SinSentido <sinsentido993@gmail.com>
2019-01-12 23:45:43 +0100

    Ejercicios 12 y 13

--- src/ejercicio12/Ejercicio12.java
+++ src/ejercicio12/Ejercicio12.java
@@ -4,7 +4,47 @@ import static keyboard.Keyboard.*;
 public class Ejercicio12 {
 	public static void main(String[] args) {
 		
+		/*
+		 * 1. Se le pide una cadena al usuario.
+		 * 2. Se le pide una segunda cadena al usuario para buscar dentro de la primera cadena.
+		 * 3. Se recorre la primera cadena hasta que se encuentra una coincidencia con el primer caracter de la segunda cadena.
+		 * 4. Cuando se encuentre la coincidencia se comprueba si los caracteres siguientes coinciden con los de la segunda cadena.
+		 * 5. Si la coincidencia de todos los carácteres es correcta se suma 1.
+		 * 6. Se muestra la suma total de coincidencias.
+		 * */
+		
+		String s1, s2;
+		
+		//1. Se le pide una cadena al usuario.
+		System.out.println("Introuduce una cadena:");
+		s1 = readString();
+		//2. Se le pide una segunda cadena al usuario para buscar dentro de la primera cadena.
+		System.out.println("Introduce una cadena para buscar dentro de la cadena anterior:");
+		s2 = readString();
+		
+		//6. Se muestra la suma total de coincidencias.
+		System.out.println(searcher(s1, s2));
 		
 	}
-
+	
+	public static int searcher(String mainString, String lookFor) {
+		int counter=0, subcounter;;
+		//3. Se recorre la primera cadena hasta que se encuentra una coincidencia con el primer caracter de la segunda cadena.
+		for(int i=0; i<mainString.length(); i++) {
+			if(mainString.charAt(i) == lookFor.charAt(0)) {
+				subcounter = 0;
+				//4. Cuando se encuentre la coincidencia se comprueba si los caracteres siguientes coinciden con los de la segunda cadena.
+				for(int j=0; j<lookFor.length(); j++, i++) {
+					if(lookFor.charAt(j) == mainString.charAt(i)) {
+						subcounter++;
+					}
+				}
+				//5. Si la coincidencia de todos los carácteres es correcta se suma 1.
+				if(subcounter == lookFor.length()) {
+					counter++;
+				}
+			}
+		}
+		return counter;
+	}
 }
--- src/ejercicio13/Ejercicio13.java
+++ src/ejercicio13/Ejercicio13.java
@@ -3,8 +3,60 @@ import static keyboard.Keyboard.*;
 
 public class Ejercicio13 {
 	public static void main(String[] args) {
+	
+		/*
+		 * 1. Se le pide al usuario dos cadenas para contar palabras con los dos metodos. La seguna cadena debe tener más de un
+		 * 	espacio seguido.
+		 * 2. Se cuentan las palabras de dos maneras.
+		 * 	2.1. La primera se reccore la cadena hasta que se encuentre un espacio y se suma 1 al contador.
+		 *  2.2. La segunda opción se recorre la cadena y cada vez que se encuentre un caracter seguido de un espacio
+		 *  se considerará que es una palabra y se sumará 1 al contador.
+		 * 3. Se muestra el resultado con las dos maneras de contar palabras.
+		 * */
 		
+		String s, s2;
+		
+		/*1. Se le pide al usuario dos cadenas para contar palabras con los dos metodos. La seguna cadena debe tener más de un
+		 espacio seguido.*/
+		System.out.println("Escribe una cadena para contar sus palabras: ");
+		s = readString();
+		System.out.println("Escribe una segunda cadena con más de un espacio seguido:");
+		s2 = readString();
+		
+		//3. Se muestra el resultado con las dos maneras de contar palabras.
+		System.out.println(wordCounter(s));
+		System.out.println(wordCounterSpaces(s2));
 		
 	}
-
+	
+	//2.1. La primera se reccore la cadena hasta que se encuentre un espacio y se suma 1 al contador.
+	public static int wordCounter(String s) {
+		int counter = 0;
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i) == ' ') {
+				counter++;
+			}
+		}
+		//Aqui siempre que la cadena no acabe en espacio suma uno al contador porque si no nunca contaría la última palabra.
+		if(s.charAt(s.length()-1) != ' ') {
+			counter++;
+		}
+		return counter;
+	}
+	
+	/*2.2. La segunda opción se recorre la cadena y cada vez que se encuentre un caracter seguido de un espacio
+	 se considerará que es una palabra y se sumará 1 al contador.*/
+	public static int wordCounterSpaces(String s) {
+		int counter = 0;
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i) == ' ' && s.charAt(i-1) != ' ') {
+				counter++;
+			}
+		}
+		//Aqui siempre que la cadena no acabe en espacio suma uno al contador porque si no nunca contaría la última palabra.
+		if(s.charAt(s.length()-1) != ' ') {
+			counter++;
+		}
+		return counter;
+	}
 }
